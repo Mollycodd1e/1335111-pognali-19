55ada16d7911575a9f3b0f14bea1da9cdd2e4ded
Mollycodd1e <selik7@mail.ru>
2020-04-29 05:57:49 +0300

    Добавление последних эффектов для index

--- source/js/menu-button.js
+++ source/js/menu-button.js
@@ -1,5 +1,6 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var pageHead = document.querySelector('.page-header');
 
   navMain.classList.remove('main-nav--nojs');
 
@@ -7,7 +8,11 @@ var navToggle = document.querySelector('.main-nav__toggle');
     if (navMain.classList.contains('main-nav--closed')){
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--opened');
+      blueLogo();
     } else {
       navMain.classList.remove('main-nav--opened');
       navMain.classList.add('main-nav--closed');
+      if (pageHead.classList.contains('page-header--scrolled') === false) {
+        whiteLogo();
+      }
   }});
--- source/js/scroll-menu.js
+++ source/js/scroll-menu.js
@@ -1,10 +1,24 @@
-var nav = document.querySelector('.main-nav');
-window.addEventListener('scroll', function(event) {
-    event.preventDefault();
+var pageScroll = document.querySelector('.page-header');
+var mainNavScroll = document.querySelector('.main-nav');
 
-    if (window.scrollY < 74) {
-      nav.classList.remove('main-nav--scrolled');
-    } else {
-      nav.classList.add('main-nav--scrolled');
-    }
+window.addEventListener('scroll', function() {
+  if (window.innerWidth < 1440) {
+    mainNavScroll.classList.add('main-nav--scrolled');
+    pageScroll.classList.add('page-header--scrolled');
+    blueLogo();
+
+  } else
+
+  if (window.innerWidth >= 1440 && window.pageYOffset >= 750) {
+    mainNavScroll.classList.add('main-nav--scrolled');
+    pageScroll.classList.add('page-header--scrolled');
+    blueLogo();
+  }
+
+  if ((window.pageYOffset === 0 && mainNavScroll.classList.contains('.main-nav--opened') === false) || (window.innerWidth >= 1440 && window.pageYOffset < 750)) {
+    mainNavScroll.classList.remove('main-nav--scrolled');
+    pageScroll.classList.remove('page-header--scrolled');
+      whiteLogo();
+  }
 });
+
