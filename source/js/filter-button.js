3bfe3f7585c240654725f0f4812778bae523e87c
Mollycodd1e <selik7@mail.ru>
2020-05-14 16:31:39 +0300

    правка

--- source/js/filter-button.js
+++ source/js/filter-button.js
@@ -5,6 +5,7 @@ var filterClose = document.querySelector('.country-filter__close');
   filterMain.classList.remove('country-filter--nojs');
 
   filterToggle.addEventListener('click', function() {
+    evt.preventDefault();
     if (filterMain.classList.contains('country-filter--closed')) {
       filterMain.classList.remove('country-filter--closed');
       filterMain.classList.add('country-filter--opened');
