8e3b04d54b680816f9eaeae1ed41aa770c1f5d22
Mollycodd1e <selik7@mail.ru>
2020-05-18 20:50:00 +0300

    Завершение верстки

--- source/js/textarea.js
+++ source/js/textarea.js
@@ -1,15 +1,19 @@
-var formSubmit = document.querySelector('.companion-form');
-var formInput = document.querySelector('.companion-form__entertainment-input');
+var formInputBosnia = document.querySelector('.companion-form__entertainment-input--bosnia');
 var formSubmitButton = document.querySelector('.companion-form__form-button');
 var showError = document.querySelector('.companion-form__error');
 
+formInputBosnia.addEventListener("focus", function () {
+  formInputBosnia.classList.remove('companion-form__entertainment-input--error');
+  showError.classList.remove('companion-form__error--show');
+}),
+
 formSubmitButton.addEventListener("click", function (evt) {
-  if (!formInput.textContent) {
+  if (!formInputBosnia.textContent) {
     evt.preventDefault();
-    formInput.classList.add('companion-form__entertainment-input--error');
+    formInputBosnia.classList.add('companion-form__entertainment-input--error');
     showError.classList.add('companion-form__error--show');
   } else {
-    formInput.classList.remove('companion-form__entertainment-input--error');
+    formInputBosnia.classList.remove('companion-form__entertainment-input--error');
     showError.classList.remove('companion-form__error--show');
   }
 });
