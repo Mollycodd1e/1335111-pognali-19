4aae2c7e501bca9148d84c45ce2bc8d38c057112
Mollycodd1e <selik7@mail.ru>
2020-05-18 14:27:08 +0300

    Исправление ошибок в IE

--- source/js/textarea.js
+++ source/js/textarea.js
@@ -1,13 +1,8 @@
 var formSubmit = document.querySelector('.companion-form');
-var formInput = document.querySelector('.companion-form__entertainment-input');
+var formInput = document.querySelector('.companion-form__entertainment-input--bosnia');
 var formSubmitButton = document.querySelector('.companion-form__form-button');
 
-formSubmitButton.addEventListener("submit", function (evt) {
+formSubmitButton.addEventListener("click", function (evt) {
   if (!formInput.value) {
     evt.preventDefault();
-    formInput.classList.add('companion-form__entertainment-input--error');
-    formInput.placeholder = 'Введите e-mail';
-  } else {
-    formInput.classList.remove('companion-form__entertainment-input--error');
-  }
-});
+}});
